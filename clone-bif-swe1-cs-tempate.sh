bde76252d65a78645da052249d72875940502be1
Arthur Zaczek <zaczek@technikum-wien.at>
2014-09-24 14:03:05 +0200

    Adding linux scripts

---
+++ clone-bif-swe1-cs-tempate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ "$#" -ne 4 ]; then
+  echo "use: ./clone-bif-swe1-cs-tempate.sh BIF-WS??-SWE1 <if-nummer hauptrepository> <if-nummer zweitrepository> <if-nummer ihres Benutzers>"
+  exit 1;
+fi
+
+repoName=$1
+
+echo "Cloning template"
+echo "================"
+
+git clone https://inf-swe.technikum-wien.at/r/BIF/SWE1-CS.git "$repoName"
+cd "$repoName"
+chmod +x *.sh
+./setup-remotes.sh $repoName $mainRepoUserName $secondaryRepoUserName $myUserName
---
+++ setup-remotes.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ "$#" -ne 4 ]; then
+  echo "use: ./clone-bif-swe1-cs-tempate.sh BIF-WS??-SWE1 <if-nummer hauptrepository> <if-nummer zweitrepository> <if-nummer ihres Benutzers>"
+  exit 1;
+fi
+
+repoName=$1
+mainRepoUserName=$2
+secondaryRepoUserName=$3
+myUserName=$4
+
+echo "Setting up remotes"
+echo "=================="
+
+echo "Repo Name: $repoName"
+echo "Main:      $mainRepoUserName"
+echo "Secondary: $secondaryRepoUserName"
+echo "Username:  $myUserName"
+
+git remote set-url origin "https://$myUserName@inf-swe.technikum-wien.at/r/~$mainRepoUserName/$repoName.git"
+
+git remote remove $secondaryRepoUserName
+git remote remove all
+
+git remote add $secondaryRepoUserName https://$myUserName@inf-swe.technikum-wien.at/r/~$secondaryRepoUserName/$repoName.git
+git remote add all "https://$myUserName@inf-swe.technikum-wien.at/r/~$mainRepoUserName/$repoName.git"
+git remote set-url --add all "https://$myUserName@inf-swe.technikum-wien.at/r/~$secondaryRepoUserName/$repoName.git"
+
+echo "Result:"
+git remote -v
