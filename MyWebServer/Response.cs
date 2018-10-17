aa34a1290de4d460294a9ccfa8c69ba617ac816a
Alirez14 <43641705+Alirez14@users.noreply.github.com>
2018-10-17 17:03:43 +0200

    response added

---
+++ MyWebServer/Response.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace BIF.SWE1.Interfaces
+{
+    public class Response : IResponse
+    {
+        public Response()
+        {
+
+        }
+        /// <summary>
+        /// Returns a writable dictionary of the response headers. Never returns null.
+        /// </summary>
+        public IDictionary<string, string> Headers { get; }
+
+        /// <summary>
+        /// Returns the content length or 0 if no content is set yet.
+        /// </summary>
+       public int ContentLength { get; }
+
+        /// <summary>
+        /// Gets or sets the content type of the response.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">A specialized implementation may throw a InvalidOperationException when the content type is set by the implementation.</exception>
+        public string ContentType { get; set; }
+
+        /// <summary>
+        /// Gets or sets the current status code. An Exceptions is thrown, if no status code was set.
+        /// </summary>
+        public int StatusCode { get; set; }
+        /// <summary>
+        /// Returns the status code as string. (200 OK)
+        /// </summary>
+        public string Status { get; }
+
+        /// <summary>
+        /// Adds or replaces a response header in the headers dictionary.
+        /// </summary>
+        /// <param name="header"></param>
+        /// <param name="value"></param>
+        public void AddHeader(string header, string value) { }
+
+        /// <summary>
+        /// Gets or sets the Server response header. Defaults to "BIF-SWE1-Server".
+        /// </summary>
+        public string ServerHeader { get; set; }
+
+        /// <summary>
+        /// Sets a string content. The content will be encoded in UTF-8.
+        /// </summary>
+        /// <param name="content"></param>
+        public void SetContent(string content)
+        {
+
+        }
+        /// <summary>
+        /// Sets a byte[] as content.
+        /// </summary>
+        /// <param name="content"></param>
+        public void SetContent(byte[] content)
+        {
+
+        }
+        /// <summary>
+        /// Sets the stream as content.
+        /// </summary>
+        /// <param name="stream"></param>
+        public void SetContent(Stream stream)
+        {
+
+        }
+
+        /// <summary>
+        /// Sends the response to the network stream.
+        /// </summary>
+        /// <param name="network"></param>
+        public void Send(Stream network)
+        {
+
+        }
+    }
+}
