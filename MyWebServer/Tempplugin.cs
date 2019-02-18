e6ccdc5092b9200695891e63aab7baa00e2152b3
Unknown <alireza_esi14@yahoo.com>
2019-02-18 11:56:30 +0100

    temp done
    temp done

--- MyWebServer/Tempplugin.cs
+++ MyWebServer/Tempplugin.cs
@@ -6,6 +6,13 @@ using System.Text;
 using BIF.SWE1.Interfaces;
 using System.Data.SqlClient;
 using System.IO;
+using System.Globalization;
+using System.Reflection;
+using System.Runtime.Remoting.Messaging;
+using System.Web.UI.WebControls;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Xsl;
 
 namespace MyWebServer
 {
@@ -19,75 +26,184 @@ namespace MyWebServer
                 return 1f;
             }
 
+            if (req.Url.RawUrl.Contains("GetTemperature"))
+            {
+                return 1f;
+            }
+
             return 0.0f;
         }
 
         public IResponse Handle(IRequest req)
         {
             var resp = new Response();
-            Connection sql = new Connection("GetTemp");
-            var con = sql.ConnectionString;
-
-            SqlCommand command = sql.SqlCommand;
-            List<string> data = new List<string>();
-
-            if (CanHandle(req) != 0.0f)
+            if (CanHandle(req) != 0)
             {
-                string[] split = req.Url.RawUrl.Split('?');
-                string[] getContent = split[1].Split('&');
-                string[] date = getContent[0].Split('=');
-                date[1] = date[1].Replace("\"", "'");
-               
+                //Get the Date from the URL 
+
+                #region FindUrl
+
+                string date = String.Empty;
+
+                if (req.Url.RawUrl.Contains("date="))
+                {
+                    string[] split = req.Url.RawUrl.Split('?');
+                    string[] getContent = split[1].Split('&');
+                    string[] dateSplit = getContent[0].Split('=');
+                    date = dateSplit[1];
+                }
+
+                if (req.Url.RawUrl.Contains("GetTemperature"))
+                {
+                    string[] split = req.Url.RawUrl.Split('/');
+                    date = split[2];
+                }
+
+                #endregion
+
+                //Find The Path 
+
+                #region FindPath
+
+                string path = String.Empty;
+                string type = String.Empty;
+
+                if (req.Url.RawUrl.Contains("date"))
+                {
+                    path = req.Url.Path;
+                    path = path.Substring(1);
+                    type = path.Substring(path.IndexOf('.'));
+                }
+
+                #endregion
+
+                //Create a SQL Connection
+
+                #region Connection
+
+                var con = new SqlConnection(
+                    @"Data Source=DESKTOP-6JC714Q\SQLEXPRESS;Initial Catalog=dotnet;Integrated Security=True");
+                var command = new SqlCommand("GetTemp", con);
                 command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@DATE", date);
 
-                var param = command.Parameters.Add("@DATE", con);
-                param.Value = date[1];
+                #endregion
 
+                //Save data into Xml
 
-                string path = req.Url.Path;
-                path = path.Substring(1);
-                string type = path.Substring(path.IndexOf('.'));
+                #region SaveXml
 
-                con.Open();
-                SqlDataReader reader = command.ExecuteReader();
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+
+                if (File.Exists(@"C:\Users\sina_\OneDrive\Desktop\sina\SWEPROJ\Website\temp.xml"))
+                {
+
+                    File.Delete(@"C:\Users\sina_\OneDrive\Desktop\sina\SWEPROJ\Website\temp.xml");
+                }
+
+                XmlDocument xml = new XmlDocument();
+                var reader = command.ExecuteReader();
                 if (reader.HasRows)
                 {
+                    XmlNode root = xml.CreateElement("Temperatur");
+                    xml.AppendChild(root);
+
+                    XmlNode weather = xml.CreateElement("Weather");
+                    root.AppendChild(weather);
+
                     while (reader.Read())
                     {
-                        data.Add(reader[0].ToString());
+
+                        XmlNode day = xml.CreateElement("date");
+                        day.InnerText = reader[0].ToString().Split(' ')[0];
+                        weather.AppendChild(day);
+
+                        XmlNode temp = xml.CreateElement("temp");
+                        temp.InnerText = reader[1].ToString();
+                        weather.AppendChild(temp);
+
+                        XmlNode status = xml.CreateElement("status");
+                        status.InnerText = reader[2].ToString();
+                        weather.AppendChild(status);
+
                     }
-                }
-                else
-                {
-                    resp.StatusCode = 200;
-                    resp.SetContent("No Content");
-                    return resp;
+
+                    root.AppendChild(weather);
+
+                    xml.Save(@"C:\Users\sina_\OneDrive\Desktop\sina\SWEPROJ\Website\temp.xml");
+
                 }
 
-                if (File.Exists(path))
+                else
                 {
+                    resp.StatusCode = 404;
+                    resp.SetContent("No Data has been found");
+                    return resp;
+                }
+                #endregion
+
+                //Load the Xml File && set Content
+
+                #region LoadXml & SetContent      
+
+                string content = String.Empty;
+
+                if (req.Url.RawUrl.Contains("date=") && File.Exists(path))
+                {
+
                     try
                     {
                         resp.StatusCode = 200;
                         using (FileStream fs = new FileStream(path, FileMode.Open))
                         {
-                            string content = string.Empty;
+                            //string content = string.Empty;
                             StreamReader read = new StreamReader(fs);
                             while (!read.EndOfStream)
                             {
                                 content += read.ReadLine();
                             }
 
-                            content = content.Replace("</body>", "<br>");
-                            content += data[0];
+                            List<string> xmlToString = new List<string>();
+                            using (XmlTextReader xmlReader = new XmlTextReader(@"C:\Users\sina_\OneDrive\Desktop\sina\SWEPROJ\Website\temp.xml"))
+                            {
+                                xmlReader.WhitespaceHandling = WhitespaceHandling.None;
+                                xmlReader.MoveToContent();
+                                xmlToString.Clear();
+
+                                while (xmlReader.Read())
+                                {
+                                    if (xmlReader.Value == "")
+                                    {
+                                        continue;
+                                    }
+
+                                    xmlToString.Add(xmlReader.Value);
+                                }
+                            }
+
+                            if (xmlToString.Count != 0)
+                            {
+                                string[] javaArray = xmlToString.ToArray();
+
+                                content += "<table><tr><th>Date</th><th>Temperature</th><th>Status</th>" +
+                                           "<tr><td>" + xmlToString.First() + "</td>" +
+                                           "<td>" + xmlToString[1] + "</td>" + "<td>" + xmlToString[2] + "</td></tr></table>" +
+                                           "<br> <button style = \"margin:auto; display:block;\" onclick = nextResult(" + javaArray + ")" + ">Next Result </button>";
+                            }
+
+                            else
+                            {
+                                resp.SetContent("Not Found");
+                                return resp;
+                            }
+
+                            resp.ContentType = type;
                             resp.SetContent(content);
-                            
+                            return resp;
                         }
-
-                        resp.contenttype = Response.Typepmap[type];
-
-                        
-                        return resp;
                     }
                     catch
                     {
@@ -95,10 +211,28 @@ namespace MyWebServer
                         return resp;
                     }
                 }
+
+                else if (req.Url.RawUrl.Contains("GetTemperature") && File.Exists(@"C:\Users\sina_\OneDrive\Desktop\sina\SWEPROJ\Website\temp.xml"))
+                {
+                    var xmlDoc = XElement.Load(@"C:\Users\sina_\OneDrive\Desktop\sina\SWEPROJ\Website\temp.xml");
+                    content += xmlDoc;
+                    resp.SetContent(content);
+                    resp.ContentType = "text/xml";
+                    resp.StatusCode = 200;
+                    return resp;
+                }
+                else
+                {
+                    resp.StatusCode = 200;
+                    resp.SetContent("Data Has not been Found, Pick another date");
+                    return resp;
+                }
+
             }
-            return resp;
+            #endregion
+
+            return null;
 
         }
     }
 }
-
